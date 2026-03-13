ac6127db8fb4fc4e59bc66e666fd1270919a0b89
Jeremy Margaritondo <jmargari@gmail.com>
2026-03-12 21:50:37 -0500

    Initial Commit

---
+++ AtelierCode/AtelierCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtelierCodeApp.swift
+//  AtelierCode
+//
+//  Created by Jeremy Margaritondo on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct AtelierCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtelierCodeTests/AtelierCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtelierCodeTests.swift
+//  AtelierCodeTests
+//
+//  Created by Jeremy Margaritondo on 3/12/26.
+//
+
+import Testing
+@testable import AtelierCode
+
+struct AtelierCodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
