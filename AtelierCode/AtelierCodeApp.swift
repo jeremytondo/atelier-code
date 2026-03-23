3b2cec120697675efb5aed0f9882a0a94e4c5bc0
Jeremy Margaritondo <jmargari@gmail.com>
2026-03-23 13:28:44 -0700

    Track AtelierCode as a normal directory

---
+++ AtelierCode/AtelierCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtelierCodeApp.swift
+//  AtelierCode
+//
+//  Created by Jeremy Margaritondo on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct AtelierCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
